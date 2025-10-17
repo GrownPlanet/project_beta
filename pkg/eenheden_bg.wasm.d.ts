5b1c16c2a662b5a46ab74cb499676aed4502a95b
Timo dh <timo.dehandschutter@gmail.com>
2025-10-17 20:32:14 +0200

    add pkg folder

---
+++ pkg/eenheden_bg.wasm.d.ts
@@ -0,0 +1,15 @@
+/* tslint:disable */
+/* eslint-disable */
+export const memory: WebAssembly.Memory;
+export const get_time_formats: () => [number, number];
+export const get_distance_formats: () => [number, number];
+export const convert_seconds: (a: number, b: number, c: number) => [number, number];
+export const convert_meters: (a: number, b: number, c: number) => [number, number];
+export const convert_speed: (a: number, b: number, c: number, d: number, e: number) => [number, number];
+export const convert_acceleration: (a: number, b: number, c: number, d: number, e: number) => [number, number];
+export const __wbindgen_export_0: WebAssembly.Table;
+export const __externref_drop_slice: (a: number, b: number) => void;
+export const __wbindgen_free: (a: number, b: number, c: number) => void;
+export const __wbindgen_malloc: (a: number, b: number) => number;
+export const __wbindgen_realloc: (a: number, b: number, c: number, d: number) => number;
+export const __wbindgen_start: () => void;
